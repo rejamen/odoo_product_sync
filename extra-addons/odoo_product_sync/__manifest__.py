c5d129c8c1fafc5bf236758eac4e7ab60e40680b
Reinaldo J. Menendez Alonso <rejamen@gmail.com>
2019-08-12 09:03:44 -0400

    Add connector dependency

--- extra-addons/odoo_product_sync/__manifest__.py
+++ extra-addons/odoo_product_sync/__manifest__.py
@@ -28,7 +28,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','stock'],
+    'depends': ['base','stock','connector'],
 
     # always loaded
     'data': [
